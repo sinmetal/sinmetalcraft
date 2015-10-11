5ddb2a97c08313068436bb4bc8d2ff94fd288b62
Shingo Ishimura <metal.tie@gmail.com>
2015-10-11 22:59:43 +0900

    shutdown scriptで、worldをtarで固める前に、minecraftを停止するようにした

--- minecraftserver-shutdown-script.sh
+++ minecraftserver-shutdown-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-screen -r mcs -X stuff '/save-all\n/save-off\n'
+sudo screen -r -X stuff '/stop\n'
 cd /home/minecraft
 sudo tar cvf world.tar world
 WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Metadata-Flavor: Google")
@@ -7,5 +7,4 @@ GCSPATH="gs://sinmetalcraft-minecraft-world/"
 TAR=".tar"
 WORLD_PATH=$GCSPATH$WORLD$TAR
 /usr/local/bin/gsutil cp world.tar $WORLD_PATH
-screen -r mcs -X stuff '/save-on\n'
 sudo rm world.tar
