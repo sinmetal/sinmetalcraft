73ddb616ec85d006f859da55dda96960a4118823
Shingo Ishimura <metal.tie@gmail.com>
2015-11-09 21:06:59 +0900

    shell: NEARLINE storageでParallel Composite Uploadsすると遅いので、閾値をかなり大きくした
    https://cloud.google.com/storage/docs/gsutil/commands/cp#parallel-composite-uploads

--- minecraftserver-shutdown-script.sh
+++ minecraftserver-shutdown-script.sh
@@ -6,5 +6,5 @@ WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Me
 GCSPATH="gs://sinmetalcraft-minecraft-world/"
 TAR=".tar"
 WORLD_PATH=$GCSPATH$WORLD$TAR
-/usr/local/bin/gsutil cp world.tar $WORLD_PATH
+/usr/local/bin/gsutil -o GSUtil:parallel_composite_upload_threshold=1024M cp world.tar $WORLD_PATH
 sudo rm world.tar
--- minecraftserver-startup-script.sh
+++ minecraftserver-startup-script.sh
@@ -4,7 +4,7 @@ GCSPATH="gs://sinmetalcraft-minecraft-world/"
 TAR=".tar"
 cd /home/minecraft
 WORLD_PATH=$GCSPATH$WORLD$TAR
-sudo gsutil cp $WORLD_PATH world.tar
+sudo gsutil -o GSUtil:parallel_composite_upload_threshold=1024M cp $WORLD_PATH world.tar
 sudo tar xvf world.tar
 sudo rm world.tar
 screen -d -m -S mcs java -Xms1G -Xmx7G -d64 -jar minecraft_server.1.8.8.jar nogui
