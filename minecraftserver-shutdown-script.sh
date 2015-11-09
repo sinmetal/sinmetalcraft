3ad1b97bdc65dcf2489c52223368cc3b20f1b369
Shingo Ishimura <metal.tie@gmail.com>
2015-11-09 23:19:51 +0900

    storage: nierlineだとstartup scriptでworld取ってくるのが間に合わなかったので、draにした

--- minecraftserver-shutdown-script.sh
+++ minecraftserver-shutdown-script.sh
@@ -3,8 +3,8 @@ sudo screen -r -X stuff '/stop\n'
 cd /home/minecraft
 sudo tar cvf world.tar world
 WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Metadata-Flavor: Google")
-GCSPATH="gs://sinmetalcraft-minecraft-world/"
+GCSPATH="gs://sinmetalcraft-minecraft-world-dra/"
 TAR=".tar"
 WORLD_PATH=$GCSPATH$WORLD$TAR
-/usr/local/bin/gsutil -o GSUtil:parallel_composite_upload_threshold=1024M cp world.tar $WORLD_PATH
+/usr/local/bin/gsutil cp world.tar $WORLD_PATH
 sudo rm world.tar
