acec7f2cb681497dd1ca4329e8a25d772acbf7fb
Shingo Ishimura <metal.tie@gmail.com>
2015-11-14 18:26:22 +0900

    server: crontabでのbackupに対応したcustom imageにupdate

--- appengine/sinmetalcraft/src/sinmetalcraft/sinmetalcraft.go
+++ appengine/sinmetalcraft/src/sinmetalcraft/sinmetalcraft.go
@@ -411,7 +411,7 @@ func createInstance(ctx context.Context, is *compute.InstancesService, minecraft
 				DeviceName: name,
 				Mode:       "READ_WRITE",
 				InitializeParams: &compute.AttachedDiskInitializeParams{
-					SourceImage: "https://www.googleapis.com/compute/v1/projects/" + PROJECT_NAME + "/global/images/minecraft-image-v20151113a",
+					SourceImage: "https://www.googleapis.com/compute/v1/projects/" + PROJECT_NAME + "/global/images/minecraft-image-v20151114a",
 					DiskType:    "https://www.googleapis.com/compute/v1/projects/" + PROJECT_NAME + "/zones/" + minecraft.Zone + "/diskTypes/pd-ssd",
 					DiskSizeGb:  100,
 				},
