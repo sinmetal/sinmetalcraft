833819a53aa08527b013eb4390d1eced57c8f363
Shingo Ishimura <metal.tie@gmail.com>
2015-11-14 19:31:37 +0900

    server: backup.shを叩いてtarに固めるとfileをロックしちゃうっぽいので、いったんコメントアウト

--- minecraftserver-backup.sh
+++ minecraftserver-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-screen -r mcs -X stuff '/save-all\n/save-off\n'
-sudo tar cvf world.tar world
-WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Metadata-Flavor: Google")
-GCSPATH="gs://sinmetalcraft-minecraft-world-dra/"
-TAR=".tar"
-WORLD_PATH=$GCSPATH$WORLD$TAR
-/usr/local/bin/gsutil cp world.tar $WORLD_PATH
-sudo rm world.tar
+# screen -r mcs -X stuff '/save-all\n/save-off\n'
+# sudo tar cvf world.tar world
+# WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Metadata-Flavor: Google")
+# GCSPATH="gs://sinmetalcraft-minecraft-world-dra/"
+# TAR=".tar"
+# WORLD_PATH=$GCSPATH$WORLD$TAR
+# /usr/local/bin/gsutil cp world.tar $WORLD_PATH
+# sudo rm world.tar
