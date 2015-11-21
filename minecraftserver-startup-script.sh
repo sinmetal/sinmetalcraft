c227e7c3bfed8e34568fe5baa83f1c9f638fee90
Shingo Ishimura <metal.tie@gmail.com>
2015-11-21 17:56:56 +0900

    server: 巻き戻り対策のため、インスタンスの削除cronの停止と、startup時にworldを復元するのを止めた

--- minecraftserver-startup-script.sh
+++ minecraftserver-startup-script.sh
@@ -13,10 +13,10 @@ WORLD=$(curl http://metadata/computeMetadata/v1/instance/attributes/world -H "Me
 GCSPATH="gs://sinmetalcraft-minecraft-world-dra/"
 TAR=".tar"
 WORLD_PATH=$GCSPATH$WORLD$TAR
-sudo gsutil cp $WORLD_PATH world.tar
-sudo tar xvf world.tar
-sudo rm world.tar
-sudo rm world/session.lock
+# sudo gsutil cp $WORLD_PATH world.tar
+# sudo tar xvf world.tar
+# sudo rm world.tar
+# sudo rm world/session.lock
 screen -d -m -S mcs java -Xms1G -Xmx7G -d64 -jar minecraft_server.1.8.jar nogui
 gcloud compute instances add-metadata $HOSTNAME --zone=asia-east1-b --metadata state=exists
 gsutil cp gs://sinmetalcraft-minecraft-shell/minecraftserver-backup.sh backup.sh
