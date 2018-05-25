b728e0dc482a272e6f53e1a8e8793a7708cc6070
sinmetal <metal.tie@gmail.com>
2018-05-25 23:44:47 +0900

    startup scriptの分岐を無くした
    再起動しないと接続できないことが多いので、ためしに作成と再起動の分岐をなくした
    instance tagについてもどうせ使ってない

--- minecraftserver-startup-script.sh
+++ minecraftserver-startup-script.sh
@@ -18,14 +18,6 @@ MC_JAR=$MC_APP$MC_VERSION$JAR
 GCS_BUCKET=gs://sinmetalcraft-minecraft-jar/
 GCS_MC_JAR_PATH=$GCS_BUCKET$MC_JAR
 sudo gsutil cp $GCS_MC_JAR_PATH .
-STATE=$(curl http://metadata/computeMetadata/v1/instance/attributes/state -H "Metadata-Flavor: Google")
-echo $STATE
-if [ ${STATE} = "exists" ]; then
-  echo "EXISTS INSTNCE"
-  sudo rm world/session.lock
-  sudo screen -d -m -S mcs java -Xms1G -Xmx7G -d64 -jar $MC_JAR nogui
-  exit 0
-fi
-echo "NEW INSTNCE"
+echo "EXISTS INSTNCE"
+sudo rm world/session.lock
 sudo screen -d -m -S mcs java -Xms1G -Xmx7G -d64 -jar $MC_JAR nogui
-gcloud compute instances add-metadata $HOSTNAME --zone=asia-northeast1-b --metadata state=exists
